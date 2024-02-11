5aa52f7d4635e0869ae74e93a0616fc865c6d1da
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-02-11 12:21:54 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng16';
+  constructor() {
+    effect(() => console.log(this.actions()));
+  }
+
+  title = signal('ng16');
+  vote = signal<number>(0);
+  actions = signal<string[]>([]);
+  doubleLenghtOfActions = computed(() => this.actions().length * 2);
+
+  like() {
+    this.vote.update((oldVote) => oldVote + 1);
+    this.actions.mutate((value) => value.push('Like'));
+  }
+
+  dislike() {
+    this.vote.update((oldVote) => {
+      if (oldVote) {
+        this.actions.mutate((value) => value.push('Dislike'));
+        return oldVote - 1;
+      }
+
+      return 0;
+    });
+    console.log('dislike');
+  }
 }
