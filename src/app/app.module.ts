e4b65cdc193bebd130ed7ba6e18eb8870c0571e5
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-02-24 13:02:40 +0100

    starting the new blog project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesModule } from './courses/courses.module';
 import { GithubMembersModule } from './github-members/github-members.module';
+import { BlogModule } from './blog/blog.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GithubMembersModule } from './github-members/github-members.module';
     BrowserModule,
     AppRoutingModule,
     CoursesModule,
-    GithubMembersModule
+    GithubMembersModule,
+    BlogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
