a63940f46ed6bc616c4ebf3643352a07a8b11ba9
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-02-24 13:38:14 +0100

    components & service creation

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleCreateComponent } from './components/article-create/article-create.component';
+import { ArticleEditComponent } from './components/article-edit/article-edit.component';
+import { ArticleShowComponent } from './components/article-show/article-show.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ArticleListComponent,
+    ArticleCreateComponent,
+    ArticleEditComponent,
+    ArticleShowComponent
+  ],
   imports: [
     CommonModule
   ]
