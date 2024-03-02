217b902f98d6bc2eceb3e3fa8095e43a1fcb77a1
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-03-02 15:46:25 +0100

    making better alert with sweetalert2

--- src/app/blog/components/categories/categories.component.ts
+++ src/app/blog/components/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { CategoryService } from './../../services/category.service';
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { Category } from '../../models/category';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-categories',
@@ -69,15 +70,30 @@ export class CategoriesComponent implements OnInit {
   }
 
   delete(id: number) {
-    if (!confirm('Are you sure to delete this item ?')) {
-      this.mode.set('delete');
-      return;
-    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoryService.delete(id).subscribe((res) => {
+          this.catgories.update((categories) =>
+            categories.filter((category) => category.id !== id)
+          );
 
-    this.categoryService.delete(id).subscribe((res) => {
-      this.catgories.update((categories) =>
-        categories.filter((category) => category.id !== id)
-      );
+          Swal.fire({
+            title: 'Deleted!',
+            text: 'Your file has been deleted.',
+            icon: 'success',
+            timer: 3000,
+            timerProgressBar: true,
+          });
+        });
+      }
     });
   }
 
