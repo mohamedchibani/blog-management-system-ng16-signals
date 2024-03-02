17866d5197aa7831062c21b53c25c949e7d127ce
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-03-02 16:25:10 +0100

    receiving the tag and category lists in our forum

--- src/app/blog/components/article-create/article-create.component.ts
+++ src/app/blog/components/article-create/article-create.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { TagService } from './../../services/tag.service';
+import { CategoryService } from './../../services/category.service';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { Category } from '../../models/category';
+import { Tag } from '../../models/tag';
 
 @Component({
   selector: 'app-article-create',
   templateUrl: './article-create.component.html',
-  styleUrls: ['./article-create.component.css']
+  styleUrls: ['./article-create.component.css'],
 })
-export class ArticleCreateComponent {
+export class ArticleCreateComponent implements OnInit {
+  ngOnInit(): void {
+    this.getCategories();
+    this.getTags();
+  }
 
+  categoryService = inject(CategoryService);
+  tagService = inject(TagService);
+
+  categories = signal<Category[]>([]);
+  tags = signal<Tag[]>([]);
+
+  getCategories() {
+    this.categoryService.getAll().subscribe((res) => this.categories.set(res));
+  }
+
+  getTags() {
+    this.tagService.getAll().subscribe((res) => this.tags.set(res));
+  }
 }
